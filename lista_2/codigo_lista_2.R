63d0654cf97481c451ed102d3a0f88dc2822a24c
jpjMartinez <jpjabreugh@gmail.com>
2023-10-08 03:08:54 -0300

    adicao de codigo para solucao 1.vii em R

--- lista_2/codigo_lista_2.R
+++ lista_2/codigo_lista_2.R
@@ -8,6 +8,7 @@ library(stats)
 library(ggplot2)
 library(forecast)
 library(dplyr)
+library(ggpubr)
 
 # =========================================================================
 
@@ -19,17 +20,18 @@ acf_pacf_grid <- function(ts, ts_name, num_lags = 50) {
   
   # Create an empty 1x2 grid
   par(mfrow = c(1, 2))
-
-  # Plot ACF
-  plot(acf_result$lag, acf_result$acf, type = 'h', main = NULL, xlab = 'Lag', ylab = 'FAC', col = 'blue')
   
-  # Plot PACF
+  # Plot ACF with significance bands
+  plot(acf_result$lag, acf_result$acf, type = 'h', main = NULL, xlab = 'Lag', ylab = 'FAC', col = 'blue')
+  abline(h = c(-1.96 / sqrt(length(ts)), 1.96 / sqrt(length(ts))), col = 'red', lty = 2)
+  
+  # Plot PACF with significance bands
   plot(pacf_result$lag, pacf_result$acf, type = 'h', main = NULL, xlab = 'Lag', ylab = 'FACP', col = 'blue')
-
+  abline(h = c(-1.96 / sqrt(length(ts)), 1.96 / sqrt(length(ts))), col = 'red', lty = 2)
+  
   # Plot a title in the center of the grid
   title(main = paste('FAC / FACP -', ts_name), outer = TRUE, line = -2, cex.main = 1.2)
 }
-
 
 # Function to plot a line chart
 plot_chart <- function(x, y, x_label, y_label, title, line_color = 'blue', line_size = 1.5, smoothness = 0.2) {
@@ -108,7 +110,7 @@ plot_chart(
 # Plot the ACF and PACF of the 2ª diff. of the original series
 acf_pacf_grid(ts_diff_2, '2ª Diferença da série temporal')
 
-# --------------------------------------------------------------------
+# ---------------------------- Questão 1.iii -------------------------------
 # Initialize lists to store ARMA models and AICc values separately
 arma_list <- list()
 aic_list <- list()
@@ -132,7 +134,6 @@ for (p in 0:5) {
   }
 }
 
-
 # Create a dataframe from the two lists
 results_df <- data.frame(ARMA = unlist(arma_list), AICc = unlist(aic_list))
 
@@ -148,9 +149,59 @@ best_arma <- results_df$ARMA[min_aicc_index]
 # Print the best ARMA model order
 cat("Best ARMA Model (Minimum AICc):", best_arma, "\n")
 
+# ---------------------------- Questão 1.iv -------------------------------
+
+best_model_iii <- Arima(ts_diff_2, order=c(4, 0, 5), seasonal=c(0, 0, 0, 0))
+residuals <- residuals(best_model_iii)
+acf_pacf_grid(residuals, 'Resíduos do ARMA(4,5)')
+
+# As observed in the ACF and PACF plots of the residuals acquired 
+#   with the estimation of the model ARMA(4,5) we've seen some remaining 
+#   structure with of 12 lag-frequent starting in the lags 6 and 12
+
+adjusted_best_model_iii <- Arima(ts_diff_2, order=c(12, 0, 12), seasonal=c(0, 0, 0, 0))
+residuals <- residuals(adjusted_best_model_iii)
+acf_pacf_grid(residuals, 'Resíduos do ARMA(12,12)')
+
+# ---------------------------- Questão 1.vii -------------------------------
+
+adjusted_best_model_iii <- Arima(ts_diff_2, order=c(12, 0, 12), seasonal=c(0, 0, 0, 0))
+fitted_values <- fitted(adjusted_best_model_iii)
+
+# Plot observed and predicted time series
+plot_two_charts <- function(x1, y1, x2, y2, x_label, y_label, title) {
+  df1 <- data.frame(x = x1, y = y1)
+  df2 <- data.frame(x = x2, y = y2)
+  
+  # Create a single ggplot object with both datasets
+  stacked_plot <- ggplot() +
+    geom_line(data = df1, aes(x = x, y = y), color = '#3065ab', size = 1, alpha = 1) +
+    geom_line(data = df2, aes(x = x, y = y), color = '#b52f28', size = 1, alpha = 1) +
+    labs(x = x_label, y = y_label, title = title) +
+    theme_minimal() +
+    theme(plot.title = element_text(hjust = 0.5))
+  
+  # Show the stacked plot
+  print(stacked_plot)
+}
+
+# Assuming 'fitted_values' is a list containing the fitted values
+fitted_values <- unlist(fitted_values)
+
+# Call the plot_two_charts function
+plot_two_charts(
+  x1 = index(ts_diff_2), y1 = ts_diff_2, 
+  x2 = index(ts_diff_2), y2 = fitted_values,
+  title = 'Séries prevista e observada - Período de treinamento',
+  x_label = 'Tempo (mensal)',
+  y_label = 'Geração (GWh)'
+)
+
+
+# -------------------------------------------------------------------------
 
 # Define the file name for the Excel file (replace with your desired file path)
-excel_file <- "arma_results.xlsx"
+# excel_file <- "arma_results.xlsx"
 
 # Write the dataframe to an Excel file
 # write.xlsx(results_df, file = excel_file, sheetName = "ARMA_Results", rowNames = FALSE)
